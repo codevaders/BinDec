d5e4b62dcf9ff51f6293b22f312aec9bad0bdd56
rsqn <rsqn127@gmail.com>
2015-07-22 10:44:02 +0900

    add description

--- BinDec.c
+++ BinDec.c
@@ -5,6 +5,9 @@ void main()
 	int input;
 	
 	while(1) {
+		printf("10진수 -> 2진수 변환기\n");
+		printf("0을 입력시 프로그램이 종료됩니다.\n");
+		printf("\n");
 		printf("정수를 입력하세요 : ");
 		scanf("%d", &input);
 		if(input == 0)
--- BinDec_private.h
+++ BinDec_private.h
@@ -5,19 +5,19 @@
 #define BINDEC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.1.1"
+#define VER_STRING	"1.0.1.11"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	1
-#define VER_BUILD	1
+#define VER_BUILD	11
 #define COMPANY_NAME	"rsqn"
-#define FILE_VERSION	"1.0.1.1"
-#define FILE_DESCRIPTION	" (demical number to) binary number converter For Windows operating system"
+#define FILE_VERSION	"1.0.1.11"
+#define FILE_DESCRIPTION	"(demical number to) binary number converter For Windows operating system"
 #define INTERNAL_NAME	"BinDec"
 #define LEGAL_COPYRIGHT	"rsqn127"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"BinDec"
 #define PRODUCT_NAME	"BinDec"
-#define PRODUCT_VERSION	"1.0.1.1"
+#define PRODUCT_VERSION	"1.0.1.11"
 
 #endif /*BINDEC_PRIVATE_H*/

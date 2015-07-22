e0263c9a16ef97758effedce9b2176acbe3cc51c
rsqn <rsqn127@gmail.com>
2015-07-22 11:07:56 +0900

    edit loop part

--- BinDec_private.h
+++ BinDec_private.h
@@ -5,19 +5,19 @@
 #define BINDEC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.1.11"
+#define VER_STRING	"1.0.1.16"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	1
-#define VER_BUILD	11
+#define VER_BUILD	16
 #define COMPANY_NAME	"rsqn"
-#define FILE_VERSION	"1.0.1.11"
+#define FILE_VERSION	"1.0.1.16"
 #define FILE_DESCRIPTION	"(demical number to) binary number converter For Windows operating system"
 #define INTERNAL_NAME	"BinDec"
 #define LEGAL_COPYRIGHT	"rsqn127"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"BinDec"
 #define PRODUCT_NAME	"BinDec"
-#define PRODUCT_VERSION	"1.0.1.11"
+#define PRODUCT_VERSION	"1.0.1.16"
 
 #endif /*BINDEC_PRIVATE_H*/

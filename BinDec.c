d53699ce09543fc0c767e945c319024900fa770b
Jongmin Kim <kdzlvaids@gmail.com>
2015-07-25 22:26:46 +0900

    MAX_INPUT_LEN is not used in code now

--- BinDec.c
+++ BinDec.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>     /* strtol()  */
 #include <string.h>     /* strcpy()  */
 
+#if 0
+/* MAX_INPUT_LEN might be used in future, but not now */
 #define  MAX_INPUT_LEN  256
+#endif
 
 #define  MIN_INPUT_VAL  -32768
 #define  MAX_INPUT_VAL  32767
 
 long read_int32(void)
 {
-  static char input[MAX_INPUT_LEN];
+  static char input[0];
   long result = 0;
 
   scanf("%s", &input);
@@ -78,7 +81,7 @@ int main(int argc, char *argv[])
   {
     /* method 2: parameter passing */
 
-    static char arg[MAX_INPUT_LEN];
+    static char arg[0];
     strcpy(arg, argv[1]);
 
     input = strtol(arg, NULL, 10);
